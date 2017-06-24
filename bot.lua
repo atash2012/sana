82a6d3ea8b5ef1a5bdd9c862b041a26f3921169c
atash2012 <x.shojaei@gmail.com>
2017-06-24 15:52:53 +0430

    Add files via upload

--- bot.lua
+++ bot.lua
@@ -290,26 +290,27 @@ function get_bot (i, adigram)
                           local text = 
 [[<b>🖥   وضعیت و آمار </b>
    🔯 ربات من 🔯 
+   <b>]] .. tostring(IDadmin)
 <code>🤔👤 چت های شخصی  : </code>
-___________ <b>]] .. tostring(usrs) .. [[</b><code> چت</code>  🔜 ✅
+🅰🔜  <b>]] .. tostring(usrs) .. [[</b><code> user </code>  
 <code>🎎 گروههای معمولی : </code>
-___________ <b>]] .. tostring(gps) .. [[</b><code> گروه</code>  🔜 ✅
+🆗🔜  <b>]] .. tostring(gps) .. [[</b><code> Groups </code>  
 <code>👭👬 سوپر گروه ها : </code>
-_________ <b>]] .. tostring(sgps) .. [[</b><code> سوپرگروه</code>  🔜 ✅
+🆗🔜  <b>]] .. tostring(sgps) .. [[</b><code> Groups </code>  
 <code>⛓💾 لینک های ذخیره شده : </code>
-_________ <b>]] .. tostring(links)..[[</b><code> لینک</code>  🔜 ✅
+🆗🔜  <b>]] .. tostring(links)..[[</b><code> links </code>  
 <code>↪️لینک های استفاده شده : </code>
-_________ <b>]] .. tostring(glinks)..[[</b><code> لینک</code>  🔜 ✅
-<code>👾تعداد لینک های در انتظار تایید: </code>
-_________ <b>]] .. tostring(wlinks)..[[</b><code> لینک</code>  🔜 ✅
-<code>⏱تا عضویت بعدی با لینک : </code>
-_________ <b>]] .. tostring(s)..[[</b><code> ثانیه</code>  🔜 ✅
-<code>⏰تا تایید لینک بعدی : </code>
-_________ <b>]] .. tostring(ss)..[[</b><code> ثانیه</code>  🔜 ✅
-<code>⏲زمان فاصله بین ارسال : </code>
-___________ <b>]] .. tostring(delay)..[[</b><code> ثانیه</code>  🔜 ✅
-<code>🚦حداکثر سوپرگروه ها : </code>
-_________ <b>]] .. tostring(maxsg)..[[</b><code> سوپرگروه</code>  🔜 ✅
+🆗🔜  <b>]] .. tostring(glinks)..[[</b><code> links </code> 
+<code>📊 تعداد لینک های در انتظار تایید: </code>
+🆗🔜  <b>]] .. tostring(wlinks)..[[</b><code> links </code> 
+<code>⏱ مدت زمان تا عضویت با لینک👇 </code>
+🆗🔜  <b>]] .. tostring(s)..[[</b><code> seconds </code> 
+<code>⏰ مدت زمان تا تایید لینک بعدی⬇️ </code>
+🆗🔜  <b>]] .. tostring(ss)..[[</b><code> seconds </code> 
+<code>⏲ مدت زمان بین هر ارسال👇 </code>
+🆗🔜  <b>]] .. tostring(delay)..[[</b><code> seconds </code>  
+<code>🚦 سقف عضویت در سوپرگروه ها ⁉️ </code>
+🅰🔜  <b>]] .. tostring(maxsg)..[[</b><code> Groups </code>  
 <code>🔹💠🔹🔹💠🔹</code>
   🔧 ویرایش ⚙
 🆔Ⓜ @atash2012]]
