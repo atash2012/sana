f421be4a2763b3d1fb4d396053b8e0da02d93734
atash2012 <x.shojaei@gmail.com>
2017-08-31 23:48:08 +0430

    Add files via upload

--- bot.lua
+++ bot.lua
@@ -293,7 +293,7 @@ function get_bot (i, adigram)
 🎎 گروههای معمولی : 
 🆗➡️🚥  <b>]] .. tostring(gps) .. [[</b><code> Groups </code>  
 👭👬 سوپر گروه ها : 
-🆗📡<b>]] .. tostring(sgps) .. [[</b> Groups 📢💡
+🆗🔲<b>]] .. tostring(sgps) .. [[</b> Groups 📢🔲
 ⛓💾 لینک های ذخیره شده : 
 🆗➡️🚥  <b>]] .. tostring(links)..[[</b><code> links </code>  
 📚تعداد لینک های در انتظار عضویت:
@@ -301,7 +301,7 @@ function get_bot (i, adigram)
 📊 تعداد لینک های در انتظار تایید: 
 🆗➡️🚥  <b>]] .. tostring(wlinks)..[[</b><code> links </code> 
 ⏱ مدت زمان تا عضویت با لینک : 
-🆗➡️🚥  <b>]] .. tostring(s)..[[</b><code> seconds </code> 📢💡
+🆗➡️🚥  <b>]] .. tostring(s)..[[</b><code> seconds </code> 📢🔲
 ⏰ مدت زمان تا تایید لینک بعدی :
 🆗➡️🚥  <b>]] .. tostring(ss)..[[</b><code> seconds </code> 
 ⏲ مدت زمان بین هر ارسال : 
@@ -309,7 +309,7 @@ function get_bot (i, adigram)
 🔁سقف عضویت در سوپرگروه ها⁉️ 
 🅰🔜🚥  <b>]] .. tostring(maxsg)..[[</b><code> Groups  </code>  
   🔧 ویرایش ⚙ 
-  @MX_TAEM
+@MX_TAEM
 🆔Ⓜ @atash2012]]
 
                           return send(msg.chat_id_, 0, text)
